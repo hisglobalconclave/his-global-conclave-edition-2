9dd1614c3ae9fbaa8b62b70b10858b3f0f342c4e
hisglobalconclave <hisglobalconclave@gmail.com>
2025-10-31 11:20:59 +0530

    fix!: ecosoc asset path

--- src/components/ecosoc.tsx
+++ src/components/ecosoc.tsx
@@ -10,7 +10,7 @@ export default function Ecosoc() {
             <div className="relative lg:min-h-[650px] w-full max-w-[275px] items-center flex flex-col justify-start mx-10 mt-20 mb-8 lg:mb-0">
                 <div className="flex flex-col items-center">
                     <Image
-                        src="/his-global-conclave-edition-2/images/un_emblem.png"
+                        src="/images/un_emblem.png"
                         alt="UN Emblem"
                         width={200}
                         height={200}
@@ -30,7 +30,7 @@ export default function Ecosoc() {
                 <iframe src="https://drive.google.com/file/d/1_yA31113OcrO8sxcGYiXu5KNsaxg5SmE/preview" className="w-full min-w-[300px] max-w-[500px] h-full min-h-[700px]" />
 
                 <div className="flex justify-center text-center mt-10">
-                    <a href="/his-global-conclave-edition-2/bg-guide/ECOSOC-BG-V3.pdf" download>
+                    <a href="/bg-guide/ECOSOC-BG-V3.pdf" download>
                     <HoverBorderGradient
                         className="mono-label text-sm 2xl:text-xl"
                     >
@@ -45,19 +45,19 @@ export default function Ecosoc() {
                         name="M Sivakeerthana"
                         title="Chairperson"
                         description="Chairperson: ECOSOC"
-                        src="/his-global-conclave-edition-2/images/team_members/ecosoc-chair.webp"
+                        src="/images/team_members/ecosoc-chair.webp"
                     />
                     <ExpandableCard
                         name="Bhuvanaesh R"
                         title="Vice-Chairperson"
                         description="Vice-Chairperson: ECOSOC"
-                        src="/his-global-conclave-edition-2/images/team_members/ecosoc-vice-chair-p1.webp"
+                        src="/images/team_members/ecosoc-vice-chair-p1.webp"
                     />
                     <ExpandableCard
                         name="Akshaya Nidhi OG"
                         title="Vice-Chairperson"
                         description="Vice-Chairperson: ECOSOC"
-                        src="/his-global-conclave-edition-2/images/team_members/ecosoc-vice-chair-p2.webp"
+                        src="/images/team_members/ecosoc-vice-chair-p2.webp"
                     />
             </div>
         </div>
