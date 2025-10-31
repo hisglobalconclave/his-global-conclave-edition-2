1aa9bdc1ea0833597b61583218c3f4fe57c32eff
hisglobalconclave <hisglobalconclave@gmail.com>
2025-10-31 11:17:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/his-global-conclave-edition-2',
-  assetPrefix: '/his-global-conclave-edition-2/', // IMPORTANT for GitHub Pages
+  basePath: '/',
   images: { unoptimized: true },
 };
 
