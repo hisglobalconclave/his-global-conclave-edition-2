48c059c8d2aaaa9b794ef15f75f10a69f9339af3
hisglobalconclave <hisglobalconclave@gmail.com>
2025-10-31 11:23:35 +0530

    fix!: unsc asset path change

--- src/components/unsc.tsx
+++ src/components/unsc.tsx
@@ -10,7 +10,7 @@ export default function Unsc() {
             <div className="relative lg:min-h-[650px] w-full max-w-[275px] items-center flex flex-col justify-start mx-10 mt-20 mb-8 lg:mb-0">
                 <div className="flex flex-col items-center">
                     <Image
-                        src="/his-global-conclave-edition-2/images/un_emblem.png"
+                        src="/images/un_emblem.png"
                         alt="UN Emblem"
                         width={200}
                         height={200}
@@ -30,7 +30,7 @@ export default function Unsc() {
                 <iframe src="https://drive.google.com/file/d/1vU-jQJt0wc2TcX8CT_3Yuvv317zOHXw7/preview" className="w-full min-w-[300px] max-w-[500px] h-full min-h-[700px]" />
 
                 <div className="flex justify-center text-center mt-10">
-                    <a href="/his-global-conclave-edition-2/bg-guide/UNSC-BG-V2.pdf" download>
+                    <a href="/bg-guide/UNSC-BG-V2.pdf" download>
                     <HoverBorderGradient
                         className="mono-label text-sm 2xl:text-xl"
                     >
@@ -45,19 +45,19 @@ export default function Unsc() {
                         name="Alven Aloysius"
                         title="Chairperson"
                         description="Chairperson: UNSC"
-                        src="/his-global-conclave-edition-2/images/team_members/unsc-chair.webp"
+                        src="/images/team_members/unsc-chair.webp"
                     />
                     <ExpandableCard
                         name="Kishore E"
                         title="Vice-Chairperson"
                         description="Vice-Chairperson: UNSC"
-                        src="/his-global-conclave-edition-2/images/team_members/unsc-vice-chair-p1.webp"
+                        src="/images/team_members/unsc-vice-chair-p1.webp"
                     />
                     <ExpandableCard
                         name="Santhosh Bezaleel"
                         title="Vice-Chairperson"
                         description="Vice-Chairperson: UNSC"
-                        src="/his-global-conclave-edition-2/images/team_members/unsc-vice-chair-p2.webp"
+                        src="/images/team_members/unsc-vice-chair-p2.webp"
                     />
             </div>
         </div>
